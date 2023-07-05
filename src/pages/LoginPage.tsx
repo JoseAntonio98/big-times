8aef74751e75804a65c2b3a3fc410005972cb0dc
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-07-04 21:22:53 -0500

    feat: Login page initial design

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,17 +2,25 @@ import {
   IonButton,
   IonContent,
   IonHeader,
+  IonIcon,
   IonPage,
+  IonText,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { logoGoogle, logoApple } from "ionicons/icons";
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
 
+import WelcomeImage from "../assets/bigtimes.png";
+import "./LoginPage.css";
+
 const LoginPage: React.FC = () => {
   let history = useHistory();
+
   const handleLogin = () => {
     // Lógica de autenticación y validación
+    console.log("Hi");
 
     // Navegar a la página de Home
     history.push("/home");
@@ -20,13 +28,35 @@ const LoginPage: React.FC = () => {
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonButton onClick={handleLogin}>Login</IonButton>
+      <IonContent>
+        <div className="container">
+          <img alt="Silhouette of mountains" src={WelcomeImage} />
+          <IonText className="app-subtitle">Because we all have</IonText>
+          <IonText color="secondary" className="app-name">
+            Big Times
+          </IonText>
+
+          <div>
+            <IonButton
+              expand="block"
+              color="light"
+              className="ion-margin-bottom btn"
+              onClick={handleLogin}
+            >
+              <IonIcon slot="start" icon={logoGoogle}></IonIcon>
+              Login with Google
+            </IonButton>
+            <IonButton
+              expand="block"
+              color="dark"
+              className="btn"
+              onClick={handleLogin}
+            >
+              <IonIcon slot="start" icon={logoApple}></IonIcon>
+              Login with Apple
+            </IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
