d5a1dbf39931cb5240d1b85c26c869093d45df51
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-08-02 11:35:35 -0500

    Fix: Translation corrections

--- src/openaiConfig.ts
+++ src/openaiConfig.ts
@@ -1,10 +1,7 @@
-export const OPENAI_API_KEY = "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";
+export const OPENAI_API_KEY = "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";
 // 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
-
-
-// export const OPENAI_API_KEY = "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";
+// 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
 
 // https://platform.openai.com/account/api-keys
-// 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
 // https://openai.com/pricing
 // https://platform.openai.com/account/usage
