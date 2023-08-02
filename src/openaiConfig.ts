cef2b67fa6c035e88b98a124d87af0292651a9b2
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-08-01 20:12:21 -0500

    Task: Auth error fixed

--- src/openaiConfig.ts
+++ src/openaiConfig.ts
@@ -1,4 +1,8 @@
-export const OPENAI_API_KEY = "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";
+export const OPENAI_API_KEY = "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";
+// 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
+
+
+// export const OPENAI_API_KEY = "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";
 
 // https://platform.openai.com/account/api-keys
 // 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
