8caaf3554ee73b9157557fc4a07070536b416cf6
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-08-02 15:06:04 -0500

    Fix: Key fixed

--- src/openaiConfig.ts
+++ src/openaiConfig.ts
@@ -1,4 +1,6 @@
-export const OPENAI_API_KEY = "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";
+export const OPENAI_API_KEY = "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";
+
+// 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
 // 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
 // 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
 
