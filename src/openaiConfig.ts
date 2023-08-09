c3b17ae4915ae931e9cbee44f0e582e9fc7bee53
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-08-09 14:46:21 -0500

    Fix: Update contants

--- src/openaiConfig.ts
+++ src/openaiConfig.ts
@@ -1,4 +1,5 @@
-export const OPENAI_API_KEY = "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";
+export const OPENAI_API_KEY = "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";
+// export const OPENAI_API_KEY = "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";
 // export const OPENAI_API_KEY = "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";
 
 // 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
